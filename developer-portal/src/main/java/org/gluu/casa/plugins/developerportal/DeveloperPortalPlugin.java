b8b6534814089b2a86578da53eafc99d29eda16e
michal kepkowski <mk@gluu.org>
2018-08-20 22:40:03 +1000

    project init

---
+++ developer-portal/src/main/java/org/gluu/casa/plugins/developerportal/DeveloperPortalPlugin.java
@@ -0,0 +1,14 @@
+package org.gluu.casa.plugins.developerportal;
+
+import org.pf4j.Plugin;
+import org.pf4j.PluginWrapper;
+
+/**
+ * @author kepuss
+ */
+public class DeveloperPortalPlugin  extends Plugin {
+        public DeveloperPortalPlugin(PluginWrapper wrapper) {
+            super(wrapper);
+        }
+}
+
